eda0a58070200da520d9dd0000c6289b2d5346f0
Shubham Mandal <shubm23@gmail.com>
2026-05-25 21:35:20 +0530

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        Map<Character, Integer> s1Count = new HashMap<>(), windowCount = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count.merge(s1.charAt(i),1,Integer::sum);
+            windowCount.merge(s2.charAt(i),1,Integer::sum);
+        }
+
+        if (matches(s1Count, windowCount)) return true;
+
+        int l = 0;
+
+        for (int r = s1.length(); r < s2.length(); r++) {
+            windowCount.merge(s2.charAt(r),1,Integer::sum);
+
+            windowCount.compute(s2.charAt(l), (k, v) -> v == null || v == 0 ? 0 : v - 1);
+
+            if (windowCount.get(s2.charAt(l)) == 0) {
+                windowCount.remove(s2.charAt(l));
+            }
+            l++;
+
+            if (matches(s1Count, windowCount)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean matches(Map<Character, Integer> obj1, Map<Character, Integer> obj2) {
+        Set<Character> key1Set = obj1.keySet();
+        Set<Character> key2Set = obj2.keySet();
+        if (key1Set.size() != key2Set.size()) return false;
+        for (Character key : key1Set) {
+            if (!obj1.get(key).equals(obj2.get(key))) return false;
+        }
+        return true;
+    }
+}

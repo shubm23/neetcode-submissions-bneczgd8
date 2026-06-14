f3238e9b9ef3a852b3077cd9042b39f5ee8e3f75
Shubham Mandal <shubm23@gmail.com>
2026-06-14 21:36:08 +0530

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return Math.min(minCostClimbingStairs(cost.length - 1, cost, memo),
+            minCostClimbingStairs(cost.length - 2, cost, memo));
+    }
+
+    public int minCostClimbingStairs(int i, int[] cost, Map<Integer, Integer> memo) {
+        if (i < 0)
+            return 0;
+        if (i == 0 || i == 1)
+            return cost[i];
+        if (memo.containsKey(i))
+            return memo.get(i);
+        memo.put(i,
+            cost[i]
+                + Math.min(minCostClimbingStairs(i - 1, cost, memo),
+                    minCostClimbingStairs(i - 2, cost, memo)));
+        return memo.get(i);
+    }
+}
